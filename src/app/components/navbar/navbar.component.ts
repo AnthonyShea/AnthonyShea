83a39b07715ae7bbb4ce4b1143c464c80884bbcd
Itsuki5696 <cuijiayi2005@outlook.com>
2025-05-06 15:11:40 -0500

    Update footer

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -63,7 +63,6 @@ export class NavbarComponent implements OnInit {
         document.documentElement.setAttribute("data-bs-theme", "dark");
         document.getElementById('darkModeSwitch')?.setAttribute('checked', 'checked');
     }
-    console.log("aaaa")
     console.log(prefersDark);
   }
 
