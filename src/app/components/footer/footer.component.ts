b403ee5c298f06898aa2f3052253b23050b499cb
Itsuki5696 <cuijiayi2005@outlook.com>
2025-05-07 11:07:30 -0500

    Add footer component

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import bootstrap from 'src/js/bootstrap.js'
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    document.getElementById("shadow")!.style.display = "none";
+  }
+  
+  CiteClick(): void {
+    window.prompt('Citation', 'Abcd, E., 2025, https://mcaredb.org/');
+  }
+
+  showChangelog(): void {
+    const changelogToast = document.getElementById("changelogToast");
+    bootstrap.Toast.getOrCreateInstance(changelogToast).show();
+    document.getElementById("shadow")!.style.display = "block";
+  }
+
+  hideChangelog(): void {
+    document.getElementById("shadow")!.style.display = "none";
+  }
+}
